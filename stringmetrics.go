8b4ba0ba7e4b2d18235bfba077495ca513c756ff
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2012-04-15 19:22:46 +0100

    Removed worse performing Levenshtein implementation

--- stringmetrics.go
+++ stringmetrics.go
@@ -1,13 +1,5 @@
 package stringmetrics
 
-func makeMatrix(n, m int) [][]int {
-  matrix := make([][]int, n)
-  for i := 0; i < n; i++ {
-    matrix[i] = make([]int, m)
-  }
-  return matrix
-}
-
 func minInt(a, b, c int) int {
   min := a
   if b < min {
@@ -20,35 +12,8 @@ func minInt(a, b, c int) int {
 }
 
 func Levenshtein(a, b string) int {
-  n, m := len(a) + 1, len(b) + 1
-  d := makeMatrix(n, m)
-
-  for i := 0; i < n; i++ {
-    d[i][0] = i
-  }
-  for i := 0; i < m; i++ {
-    d[0][i] = i
-  }
-
-  for i := 1; i < n; i++ {
-    for j := 1; j < m; j++ {
-      if a[i-1] == b[j-1] {
-        d[i][j] = d[i-1][j-1]
-      } else {
-        d[i][j] = minInt(d[i-1][j] + 1, 
-          d[i][j-1] + 1, 
-          d[i-1][j-1] + 1)
-      }
-    }
-  }
-
-  return d[n-1][m-1]
-}
-
-func Levenshtein2(a, b string) int {
   n, m := len(a), len(b)
   if n > m {
-    // make sure n <= m, to use the minimum space
     a, b = b, a
     n, m = m, n
   }
