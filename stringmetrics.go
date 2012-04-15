8001969e98317454bbe2d7b5bdb1ac10952ff751
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2012-04-15 21:39:40 +0100

    Added Jaro and Jaro-Winkler metrics.

--- stringmetrics.go
+++ stringmetrics.go
@@ -1,14 +1,17 @@
 package stringmetrics
 
-func minInt(a, b, c int) int {
-  min := a
-  if b < min {
-    min = b
+func min(a, b int) int {
+  if a < b {
+    return a
   }
-  if c < min {
-    min = c
+  return b
+}
+
+func max(a, b int) int {
+  if a < b {
+    return b
   }
-  return min
+  return a
 }
 
 func Levenshtein(a, b string) int {
@@ -33,10 +36,77 @@ func Levenshtein(a, b string) int {
         add    = previous[j] + 1
         delete = current[j-1] + 1
         change = previous[j-1] + 1
-        current[j] = minInt(add, delete, change)
+        current[j] = min(min(add, delete), change)
       }
     }
   }
 
   return current[n]
+}
+
+func Jaro(a, b string) float64 {
+  n, m := len(a), len(b)
+  if n == 0 || m == 0 {
+    if n == 0 && m == 0 {
+      return 1.0
+    }
+    return 0.0
+  }
+
+  if n > m {
+    a, b = b, a
+    n, m = m, n
+  }
+
+  search_range := (n / 2) - 1
+  if search_range < 0 {
+    search_range = 0
+  }
+
+  idx1 := make([]bool, n)
+  idx2 := make([]bool, m)
+
+  match := 0
+  for i := 0; i < n; i++ {
+    start := max(0, i-search_range)
+    end   := min(i+search_range+1, m)
+    for j := start; j < end; j++ {
+      if !idx2[j] && a[i] == b[j] {
+        idx1[i] = true
+        idx2[j] = true
+        match++
+        break
+      }
+    }
+  }
+  if match == 0 { return 0.0 }
+
+  var i, j, trans int
+  for i, j, trans = 0, 0, 0; i < n; i++ {
+    if idx1[i] {
+      for !idx2[j] { j++ }
+      if a[i] != b[j] {
+        trans++
+      }
+      j++
+    }
+  }
+
+  matchf := float64(match)
+  return (matchf / float64(n) + matchf / float64(m) + float64(match - trans/2)/matchf) / 3.0
+}
+
+func JaroWinkler(a, b string) float64 {
+  weight := Jaro(a, b)
+  if weight > 0.7 {
+    i, j := 0, min(min(len(a), len(b)), 4)
+    for ; i < j; i++ {
+      if a[i] != b[i] {
+        break
+      }
+    }
+    weight += float64(i) * 0.1 * (1.0 - weight)
+  }
+
+  return weight
 }
--- stringmetrics_test.go
+++ stringmetrics_test.go
@@ -28,3 +28,43 @@ func Benchmark_Levenshtein_Long(b *testing.B) {
     Levenshtein(LOREM_ONE, LOREM_TWO)
   }
 }
+
+func Test_Jaro(t *testing.T) {
+  assert.Equals(t, Jaro("", ""), 1.0)
+  assert.Equals(t, Jaro("Brian", ""), 0.0)
+  assert.Equals(t, Jaro("Brian", "Brian"), 1.0)
+  assert.Equals(t, Jaro("Brian", "Jesus"), 0.0)
+  assert.Equals(t, Jaro("Thorkel", "Thorgier"), 0.77976190476190477)
+  assert.Equals(t, Jaro("Dinsdale", "D"), 0.70833333333333334)
+}
+
+func Test_Jaro_Long(t *testing.T) {
+  assert.Equals(t, Jaro(LOREM_ONE, LOREM_TWO), 0.9895633141148835)
+}
+
+func Benchmark_Jaro_Short(b *testing.B) {
+  for i := 0; i < b.N; i++ {
+    Jaro("Thorkel", "Thorgier")
+  }
+}
+
+func Test_JaroWinkler(t *testing.T) {
+  assert.Equals(t, JaroWinkler("", ""), 1.0)
+  assert.Equals(t, JaroWinkler("Brian", ""), 0.0)
+  assert.Equals(t, JaroWinkler("Brian", "Brian"), 1.0)
+  assert.Equals(t, JaroWinkler("Brian", "Jesus"), 0.0)
+  // slightly boosted higher values
+  assert.Equals(t, JaroWinkler("Thorkel", "Thorgier"), 0.8678571428571429)
+  assert.Equals(t, JaroWinkler("Dinsdale", "D"), 0.7375)
+
+}
+
+func Test_JaroWinkler_Long(t *testing.T) {
+  assert.Equals(t, JaroWinkler(LOREM_ONE, LOREM_TWO), 0.9937379884689301)
+}
+
+func Benchmark_JaroWinkler_Short(b *testing.B) {
+  for i := 0; i < b.N; i++ {
+    JaroWinkler("Thorkel", "Thorgier")
+  }
+}
