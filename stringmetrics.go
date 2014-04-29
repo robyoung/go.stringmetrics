2c4e03a446733be47b19c2c4514cfde9b8178932
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2014-04-29 22:26:45 +0100

    Go fmt yourself

--- stringmetrics.go
+++ stringmetrics.go
@@ -1,125 +1,131 @@
 package stringmetrics
 
 func min(a, b int) int {
-  if a < b {
-    return a
-  }
-  return b
+	if a < b {
+		return a
+	}
+	return b
 }
 
 func max(a, b int) int {
-  if a < b {
-    return b
-  }
-  return a
+	if a < b {
+		return b
+	}
+	return a
 }
 
 func Levenshtein(a, b string) int {
-  n, m := len(a), len(b)
-  if n > m {
-    a, b = b, a
-    n, m = m, n
-  }
+	n, m := len(a), len(b)
+	if n > m {
+		a, b = b, a
+		n, m = m, n
+	}
 
-  current  := make([]int, m+1)
-  previous := make([]int, m+1)
-  var i, j, add, delete, change int
+	current := make([]int, m+1)
+	previous := make([]int, m+1)
+	var i, j, add, delete, change int
 
-  for i = 1; i <= m; i++ {
-    copy(previous, current)
-    for j = 0; j <= m; j++ { current[j] = 0 }
-    current[0] = i
-    for j = 1; j <= n; j++ {
-      if a[j-1] == b[i-1] {
-        current[j] = previous[j-1]
-      } else {
-        add    = previous[j] + 1
-        delete = current[j-1] + 1
-        change = previous[j-1] + 1
-        current[j] = min(min(add, delete), change)
-      }
-    }
-  }
+	for i = 1; i <= m; i++ {
+		copy(previous, current)
+		for j = 0; j <= m; j++ {
+			current[j] = 0
+		}
+		current[0] = i
+		for j = 1; j <= n; j++ {
+			if a[j-1] == b[i-1] {
+				current[j] = previous[j-1]
+			} else {
+				add = previous[j] + 1
+				delete = current[j-1] + 1
+				change = previous[j-1] + 1
+				current[j] = min(min(add, delete), change)
+			}
+		}
+	}
 
-  return current[n]
+	return current[n]
 }
 
 func Jaro(a, b string) float64 {
-  n, m := len(a), len(b)
-  if n == 0 || m == 0 {
-    if n == 0 && m == 0 {
-      return 1.0
-    }
-    return 0.0
-  }
+	n, m := len(a), len(b)
+	if n == 0 || m == 0 {
+		if n == 0 && m == 0 {
+			return 1.0
+		}
+		return 0.0
+	}
 
-  if n > m {
-    a, b = b, a
-    n, m = m, n
-  }
+	if n > m {
+		a, b = b, a
+		n, m = m, n
+	}
 
-  search_range := (n / 2) - 1
-  if search_range < 0 {
-    search_range = 0
-  }
+	search_range := (n / 2) - 1
+	if search_range < 0 {
+		search_range = 0
+	}
 
-  idx1 := make([]bool, n)
-  idx2 := make([]bool, m)
+	idx1 := make([]bool, n)
+	idx2 := make([]bool, m)
 
-  match := 0
-  for i := 0; i < n; i++ {
-    start := max(0, i-search_range)
-    end   := min(i+search_range+1, m)
-    for j := start; j < end; j++ {
-      if !idx2[j] && a[i] == b[j] {
-        idx1[i] = true
-        idx2[j] = true
-        match++
-        break
-      }
-    }
-  }
-  if match == 0 { return 0.0 }
+	match := 0
+	for i := 0; i < n; i++ {
+		start := max(0, i-search_range)
+		end := min(i+search_range+1, m)
+		for j := start; j < end; j++ {
+			if !idx2[j] && a[i] == b[j] {
+				idx1[i] = true
+				idx2[j] = true
+				match++
+				break
+			}
+		}
+	}
+	if match == 0 {
+		return 0.0
+	}
 
-  var i, j, trans int
-  for i, j, trans = 0, 0, 0; i < n; i++ {
-    if idx1[i] {
-      for !idx2[j] { j++ }
-      if a[i] != b[j] {
-        trans++
-      }
-      j++
-    }
-  }
+	var i, j, trans int
+	for i, j, trans = 0, 0, 0; i < n; i++ {
+		if idx1[i] {
+			for !idx2[j] {
+				j++
+			}
+			if a[i] != b[j] {
+				trans++
+			}
+			j++
+		}
+	}
 
-  matchf := float64(match)
-  return (matchf / float64(n) + matchf / float64(m) + float64(match - trans/2)/matchf) / 3.0
+	matchf := float64(match)
+	return (matchf/float64(n) + matchf/float64(m) + float64(match-trans/2)/matchf) / 3.0
 }
 
 func JaroWinkler(a, b string) float64 {
-  weight := Jaro(a, b)
-  if weight > 0.7 {
-    i, j := 0, min(min(len(a), len(b)), 4)
-    for ; i < j; i++ {
-      if a[i] != b[i] {
-        break
-      }
-    }
-    weight += float64(i) * 0.1 * (1.0 - weight)
-  }
+	weight := Jaro(a, b)
+	if weight > 0.7 {
+		i, j := 0, min(min(len(a), len(b)), 4)
+		for ; i < j; i++ {
+			if a[i] != b[i] {
+				break
+			}
+		}
+		weight += float64(i) * 0.1 * (1.0 - weight)
+	}
 
-  return weight
+	return weight
 }
 
 func Hamming(a, b string) int {
-  if len(a) != len(b) {
-    return -1
-  }
-  sum := 0
-  for i := 0; i < len(a); i++ {
-    if a[i] != b[i] {
-      sum += 1
-    }
-  }
-  return sum
+	if len(a) != len(b) {
+		return -1
+	}
+	sum := 0
+	for i := 0; i < len(a); i++ {
+		if a[i] != b[i] {
+			sum += 1
+		}
+	}
+	return sum
 }
--- stringmetrics_test.go
+++ stringmetrics_test.go
@@ -1,76 +1,76 @@
 package stringmetrics
 
 import (
-  "testing"
-  "github.com/couchbaselabs/go.assert"
+	"github.com/couchbaselabs/go.assert"
+	"testing"
 )
 
 var LOREM_ONE string = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 var LOREM_TWO string = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est different."
 
 func Test_Levenshtein(t *testing.T) {
-  assert.Equals(t, Levenshtein("this is one", "this is two"), 3)
-  assert.Equals(t, Levenshtein("this is one", "this is ono"), 1)
+	assert.Equals(t, Levenshtein("this is one", "this is two"), 3)
+	assert.Equals(t, Levenshtein("this is one", "this is ono"), 1)
 }
 
 func Test_Levenshtein_Long(t *testing.T) {
-  assert.Equals(t, Levenshtein(LOREM_ONE, LOREM_TWO), 8)
+	assert.Equals(t, Levenshtein(LOREM_ONE, LOREM_TWO), 8)
 }
 
 func Benchmark_Levenshtein_Short(b *testing.B) {
-  for i := 0; i < b.N; i++ {
-    Levenshtein("this is one", "this is two")
-  }
+	for i := 0; i < b.N; i++ {
+		Levenshtein("this is one", "this is two")
+	}
 }
 
 func Benchmark_Levenshtein_Long(b *testing.B) {
-  for i := 0; i < b.N; i++ {
-    Levenshtein(LOREM_ONE, LOREM_TWO)
-  }
+	for i := 0; i < b.N; i++ {
+		Levenshtein(LOREM_ONE, LOREM_TWO)
+	}
 }
 
 func Test_Jaro(t *testing.T) {
-  assert.Equals(t, Jaro("", ""), 1.0)
-  assert.Equals(t, Jaro("Brian", ""), 0.0)
-  assert.Equals(t, Jaro("Brian", "Brian"), 1.0)
-  assert.Equals(t, Jaro("Brian", "Jesus"), 0.0)
-  assert.Equals(t, Jaro("Thorkel", "Thorgier"), 0.77976190476190477)
-  assert.Equals(t, Jaro("Dinsdale", "D"), 0.70833333333333334)
+	assert.Equals(t, Jaro("", ""), 1.0)
+	assert.Equals(t, Jaro("Brian", ""), 0.0)
+	assert.Equals(t, Jaro("Brian", "Brian"), 1.0)
+	assert.Equals(t, Jaro("Brian", "Jesus"), 0.0)
+	assert.Equals(t, Jaro("Thorkel", "Thorgier"), 0.77976190476190477)
+	assert.Equals(t, Jaro("Dinsdale", "D"), 0.70833333333333334)
 }
 
 func Test_Jaro_Long(t *testing.T) {
-  assert.Equals(t, Jaro(LOREM_ONE, LOREM_TWO), 0.9895633141148835)
+	assert.Equals(t, Jaro(LOREM_ONE, LOREM_TWO), 0.9895633141148835)
 }
 
 func Benchmark_Jaro_Short(b *testing.B) {
-  for i := 0; i < b.N; i++ {
-    Jaro("Thorkel", "Thorgier")
-  }
+	for i := 0; i < b.N; i++ {
+		Jaro("Thorkel", "Thorgier")
+	}
 }
 
 func Test_JaroWinkler(t *testing.T) {
-  assert.Equals(t, JaroWinkler("", ""), 1.0)
-  assert.Equals(t, JaroWinkler("Brian", ""), 0.0)
-  assert.Equals(t, JaroWinkler("Brian", "Brian"), 1.0)
-  assert.Equals(t, JaroWinkler("Brian", "Jesus"), 0.0)
-  // slightly boosted higher values
-  assert.Equals(t, JaroWinkler("Thorkel", "Thorgier"), 0.8678571428571429)
-  assert.Equals(t, JaroWinkler("Dinsdale", "D"), 0.7375)
+	assert.Equals(t, JaroWinkler("", ""), 1.0)
+	assert.Equals(t, JaroWinkler("Brian", ""), 0.0)
+	assert.Equals(t, JaroWinkler("Brian", "Brian"), 1.0)
+	assert.Equals(t, JaroWinkler("Brian", "Jesus"), 0.0)
+	// slightly boosted higher values
+	assert.Equals(t, JaroWinkler("Thorkel", "Thorgier"), 0.8678571428571429)
+	assert.Equals(t, JaroWinkler("Dinsdale", "D"), 0.7375)
 
 }
 
 func Test_JaroWinkler_Long(t *testing.T) {
-  assert.Equals(t, JaroWinkler(LOREM_ONE, LOREM_TWO), 0.9937379884689301)
+	assert.Equals(t, JaroWinkler(LOREM_ONE, LOREM_TWO), 0.9937379884689301)
 }
 
 func Benchmark_JaroWinkler_Short(b *testing.B) {
-  for i := 0; i < b.N; i++ {
-    JaroWinkler("Thorkel", "Thorgier")
-  }
+	for i := 0; i < b.N; i++ {
+		JaroWinkler("Thorkel", "Thorgier")
+	}
 }
 
 func Test_Hamming(t *testing.T) {
-  assert.Equals(t, Hamming("", ""), 0)
-  assert.Equals(t, Hamming("a", ""), -1) // testing strings of unequal length is undefined
-  assert.Equals(t, Hamming("Brian", "Jesus"), 5)
+	assert.Equals(t, Hamming("", ""), 0)
+	assert.Equals(t, Hamming("a", ""), -1) // testing strings of unequal length is undefined
+	assert.Equals(t, Hamming("Brian", "Jesus"), 5)
 }
