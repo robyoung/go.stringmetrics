b17fc2caff638e7ace068636b323654c50fd1c73
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2014-07-20 22:36:18 +0100

    Switch to testify
    It looks like go.assert is no more.

--- stringmetrics_test.go
+++ stringmetrics_test.go
@@ -1,7 +1,7 @@
 package stringmetrics
 
 import (
-	"github.com/couchbaselabs/go.assert"
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
@@ -9,12 +9,12 @@ var LOREM_ONE string = "Lorem ipsum dolor sit amet, consectetur adipisicing elit
 var LOREM_TWO string = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est different."
 
 func Test_Levenshtein(t *testing.T) {
-	assert.Equals(t, Levenshtein("this is one", "this is two"), 3)
-	assert.Equals(t, Levenshtein("this is one", "this is ono"), 1)
+	assert.Equal(t, Levenshtein("this is one", "this is two"), 3)
+	assert.Equal(t, Levenshtein("this is one", "this is ono"), 1)
 }
 
 func Test_Levenshtein_Long(t *testing.T) {
-	assert.Equals(t, Levenshtein(LOREM_ONE, LOREM_TWO), 8)
+	assert.Equal(t, Levenshtein(LOREM_ONE, LOREM_TWO), 8)
 }
 
 func Benchmark_Levenshtein_Short(b *testing.B) {
@@ -30,16 +30,16 @@ func Benchmark_Levenshtein_Long(b *testing.B) {
 }
 
 func Test_Jaro(t *testing.T) {
-	assert.Equals(t, Jaro("", ""), 1.0)
-	assert.Equals(t, Jaro("Brian", ""), 0.0)
-	assert.Equals(t, Jaro("Brian", "Brian"), 1.0)
-	assert.Equals(t, Jaro("Brian", "Jesus"), 0.0)
-	assert.Equals(t, Jaro("Thorkel", "Thorgier"), 0.77976190476190477)
-	assert.Equals(t, Jaro("Dinsdale", "D"), 0.70833333333333334)
+	assert.Equal(t, Jaro("", ""), 1.0)
+	assert.Equal(t, Jaro("Brian", ""), 0.0)
+	assert.Equal(t, Jaro("Brian", "Brian"), 1.0)
+	assert.Equal(t, Jaro("Brian", "Jesus"), 0.0)
+	assert.Equal(t, Jaro("Thorkel", "Thorgier"), 0.77976190476190477)
+	assert.Equal(t, Jaro("Dinsdale", "D"), 0.70833333333333334)
 }
 
 func Test_Jaro_Long(t *testing.T) {
-	assert.Equals(t, Jaro(LOREM_ONE, LOREM_TWO), 0.9895633141148835)
+	assert.Equal(t, Jaro(LOREM_ONE, LOREM_TWO), 0.9895633141148835)
 }
 
 func Benchmark_Jaro_Short(b *testing.B) {
@@ -49,18 +49,18 @@ func Benchmark_Jaro_Short(b *testing.B) {
 }
 
 func Test_JaroWinkler(t *testing.T) {
-	assert.Equals(t, JaroWinkler("", ""), 1.0)
-	assert.Equals(t, JaroWinkler("Brian", ""), 0.0)
-	assert.Equals(t, JaroWinkler("Brian", "Brian"), 1.0)
-	assert.Equals(t, JaroWinkler("Brian", "Jesus"), 0.0)
+	assert.Equal(t, JaroWinkler("", ""), 1.0)
+	assert.Equal(t, JaroWinkler("Brian", ""), 0.0)
+	assert.Equal(t, JaroWinkler("Brian", "Brian"), 1.0)
+	assert.Equal(t, JaroWinkler("Brian", "Jesus"), 0.0)
 	// slightly boosted higher values
-	assert.Equals(t, JaroWinkler("Thorkel", "Thorgier"), 0.8678571428571429)
-	assert.Equals(t, JaroWinkler("Dinsdale", "D"), 0.7375)
+	assert.Equal(t, JaroWinkler("Thorkel", "Thorgier"), 0.8678571428571429)
+	assert.Equal(t, JaroWinkler("Dinsdale", "D"), 0.7375)
 
 }
 
 func Test_JaroWinkler_Long(t *testing.T) {
-	assert.Equals(t, JaroWinkler(LOREM_ONE, LOREM_TWO), 0.9937379884689301)
+	assert.Equal(t, JaroWinkler(LOREM_ONE, LOREM_TWO), 0.9937379884689301)
 }
 
 func Benchmark_JaroWinkler_Short(b *testing.B) {
@@ -70,7 +70,7 @@ func Benchmark_JaroWinkler_Short(b *testing.B) {
 }
 
 func Test_Hamming(t *testing.T) {
-	assert.Equals(t, Hamming("", ""), 0)
-	assert.Equals(t, Hamming("a", ""), -1) // testing strings of unequal length is undefined
-	assert.Equals(t, Hamming("Brian", "Jesus"), 5)
+	assert.Equal(t, Hamming("", ""), 0)
+	assert.Equal(t, Hamming("a", ""), -1) // testing strings of unequal length is undefined
+	assert.Equal(t, Hamming("Brian", "Jesus"), 5)
 }
